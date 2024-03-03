e4ebe0d2945328e713275b15be8c4873d6180679
DiogoSilva48 <dio.goffs@hotmail.com>
2024-03-03 03:24:20 +0000

    big typo

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require("passport-local").Strategy;
-const UserModel = require("../model/user.js");
+const UserModel = require("../models/user.js");
 const bcrypt = require('bcrypt');
 
 // passport
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../model/user.js');
+const User = require('../models/user.js');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 
