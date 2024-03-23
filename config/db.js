39a73bdfd9864818f4f801309c58cb5d68fa9b2b
DiogoSilva48 <dio.goffs@hotmail.com>
2024-03-23 14:27:22 +0000

    change some stuff, load db from env

--- config/db.js
+++ config/db.js
@@ -1,31 +1,34 @@
 // db.js
 const mongoose = require('mongoose');
-const { uri } = require('./dbconfig');
+
+//Allow .env files to be loaded
+require('dotenv').config();
+const uri = process.env.DB_URI;
 
 async function connectToMongoDB() {
   try {
     await mongoose.connect(uri, {
-      serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
+      serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
     });
-    console.log("Connected to MongoDB!");
+    console.log('Connected to MongoDB!');
     return mongoose.connection;
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error('Error connecting to MongoDB:', error);
     throw error;
   }
-};
+}
 
 async function closeMongoDBConnection() {
   try {
     await mongoose.disconnect();
-    console.log("MongoDB connection closed.");
+    console.log('MongoDB connection closed.');
   } catch (error) {
-    console.error("Error closing MongoDB connection:", error);
+    console.error('Error closing MongoDB connection:', error);
     throw error;
   }
-};
+}
 
 module.exports = {
   connectToMongoDB,
-  closeMongoDBConnection
+  closeMongoDBConnection,
 };
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  server: {
+    port: 80, // Specify the port you want to use in production
+  },
+};
 
 export default nextConfig;
